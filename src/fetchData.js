a74a5146e881d01bd180b6050ddee1061e2a8755
Suzie Q <suzieqwolf@gmail.com>
2021-02-21 20:44:49 -0700

    Adds signin html/style & fetchData functionality.

---
+++ src/fetchData.js
@@ -0,0 +1,36 @@
+function fetchData() {
+
+  let guestData = fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users")
+    .then(response => response.json())
+    .then(json => {
+      return json.users;
+    })
+    .catch(error => console.log(error.message))
+
+  let bookingsData = fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings")
+    .then(response => response.json())
+    .then(json => {
+      return json.bookings;
+    })
+    .catch(error => console.log(error.message));
+
+  let roomsData = fetch("https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms")
+  .then(response => response.json())
+  .then(json => {
+    return json.rooms;
+  })
+  .catch(error => console.log(error.message));
+
+  return Promise.all([guestData, bookingsData, roomsData])
+    .then(jsonData => {
+      const data = {
+        guests: jsonData[0],
+        bookings: jsonData[1],
+        rooms: jsonData[2],
+      };
+      console.log('data: ', data);
+      return data;
+    })
+}
+
+export default fetchData;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,60 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
+/*
+  //// * INSTRUCTOR's CODE BELOW * /////////////////////////////////
+  // This is the JavaScript entry file - your code begins here
+  // Do not delete or rename this file ********
+  ///////////////////////////////////////////////////////////////////
+*/
 
-// An example of how you tell webpack to use a CSS (SCSS) file
+/*////// IMPORTS /////*/
+// stylesheets
 import './css/base.scss';
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// images
+import './images/hotel-lobby-small.jpg';
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// functions or files
+import fetchData from './fetchData.js';
+
+// classes
+/* import className from './classes/.js' */
+import Guest from './class/Guest.js';
+import Manager from './class/Manager.js';
+import Hotel from './class/Hotel.js';
+
+/*//// GLOBAL VARIABLES ////*/
+const outlook = {  
+  guests: null, 
+  bookings: null, 
+  rooms: null
+};
+/*//// ALL FUNCTIONS /////*/
+
+// EVENT LISTNERS
+
+window.onload = loadOutlook;
+console.log('outlook: ', outlook);
+
+// EVENT HANDLERS
+function loadOutlook() {
+  fetchData()
+  .then((data) => {
+    outlook.guests = data[0],
+    outlook.bookings = data[1],
+    outlook.rooms = data[2] 
+  })
+  .catch((error) => console.log(error.message));
+}
+
+
+
+
+// HELPER FUNCTIONS
+
+/*
+**** IMPORT EXAMPLES ****
+An example of how you tell webpack to use a CSS (SCSS) file:  
+import './css/base.css';
+
+An example of how you tell webpack to use an image (also need to link to it in the index.html)
+import './images/turing-logo.png';
+*/
