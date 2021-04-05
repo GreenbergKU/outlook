57313f4786b930c0c9a5b7a0b6372a22ec8767d6
Suzie Q <suzieqwolf@gmail.com>
2021-04-04 18:15:38 -0600

    Adds changes to User class.

--- src/class/User.js
+++ src/class/User.js
@@ -1,24 +1,40 @@
 class User {
-  constructor(username, password, date) {
+  constructor(username, password) {
     this.username = username;
-    this.password = password; 
-    this.date = date;
+    this.password = password;
+
+    this.type = "login";
+    this.labelInput = "username";
+    this.input = "submit";
+    this.placeHolder = "customer00";
+
     this.userID;
   };
 
   formatUser() {
-    if (this.username.includes('customer')) { 
-      this.userID = parseInt(this.username.split('customer')[1]);
-      this.username = 'guest';
-    }
+    console.log('this.username.includes("customer"): ', this.username.includes('customer'));
+    this.username.includes('customer') ? (
+      this.userID = parseInt(this.username.split('customer')[1]),
+      this.username = 'guest'
+    ) : null;
+    console.log('this @formatUser: ', this);  
     return this;
   }
 
-  validation(guestsNum) {
+  validateUser(usersTot) {
     //console.log('validation(guestsNum): ', validation(guestsNum));
-    let isValid = this.username === "guest" ? this.userID <= guestsNum : this.username === "manager";
-    return isValid
-  }
+    const validPassword = this.password === "overlook2020" ? true : alert("invalid password!") 
+    const validUsername = 
+      this.username != "manager" 
+      ? this.userID > usersTot 
+      ? alert("invalid username") : this.username === "guest" 
+      : this.username === "manager";
+    //!this.userID ? this.username != "manager" ? alert("wrong username") : this.userID > users ? alert("wrong customerID") : ;
+    return validPassword && validUsername;
+  };
+  
+
+
 
 }
 
