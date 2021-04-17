09323c39f62da4e2baf0e02eedb64fdcfa397b65
Suzie Q <suzieqwolf@gmail.com>
2021-04-17 05:53:05 -0600

    Incorporates API addBooking() and deleteBooking().

--- src/addBooking.js
+++ src/addBooking.js
@@ -1,17 +1,33 @@
-function addBooking(newBooking) {
+function addBooking(newBooking, formatDate, loadPage, update, userX) {
   console.log('newBooking: ', newBooking);
-  
-  fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
+  //let confCode;
+  let promise = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(newBooking),
   })
-    .then(response => response.json())
-    .then(json => console.log('Request success: ', json))
-    .catch(err => console.log(err.message))
-
+  .then(response => {
+    return response.json()
+  })
+  .then(json => {
+    //const confCode = json.id;
+    alert(`
+      YOUR RESERVATION HAS BEEN CONFIRMED!
+      
+      BOOKING CONFIRMATION (for your records):
+        
+      Check-in Date: ${formatDate(json.date, "numbers")}
+      Room Number: ${json.roomNumber}
+      Confirmation Code: ${json.id}
+    `);
+    return json 
+  })
+  .then(json => console.log('Request success: ', json))
+  .catch(err => console.log('err.message: ', err.message))
+  .then(loadPage())
+  .then(update(userX))
 }
 
 export default addBooking;
