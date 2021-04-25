2627bb5fdcd5b51df433b864c6881bfb7fed2147
Suzie Q <suzieqwolf@gmail.com>
2021-04-25 01:44:37 -0600

    Deletes unneccessary class files and tests.

--- src/testBookings.js
+++
@@ -1,9 +0,0 @@
-const testBookings = [
-    {"id":"5fwrgu4i7k55hl6zz1","userID":1,"date":"2021/09/22","roomNumber":15,"roomServiceCharges":[]},
-    {"id":"5fwrgu4i7k55hl6zz2","userID":1,"date":"2021/09/24","roomNumber":24,"roomServiceCharges":[]},
-    {"id":"5fwrgu4i7k55hl6zz3","userID":1,"date":"2021/09/10","roomNumber":12,"roomServiceCharges":[]}
-];
-
-export default testBookings;
-
-  
