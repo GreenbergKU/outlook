13301f36a060348d852e34a2afe7239ebdd6ab5a
Suzie Q <suzieqwolf@gmail.com>
2021-05-23 21:21:35 -0600

    Adds changes for  passing initial tests.

--- src/class/data/HotelData.js
+++ src/class/data/HotelData.js
@@ -9,15 +9,75 @@ class HotelData {
   constructor(data) {
     // console.log('data @HotelData: ', data);
 
-    this.usersData = data.usersData;
+    this.usersData = data ? data.usersData : [];
       // console.log('this.usersData: ', this.usersData);  
-    this.bookingsData = data.bookingsData;
+    this.bookingsData = data ? data.bookingsData : [];
       // console.log('this.bookingsData: ', this.bookingsData);   
-    this.roomsData = data.roomsData;
+    this.roomsData = data ? data.roomsData : [];
       // console.log('this.roomsData: ', this.roomsData);
     this.totalUsers = this.usersData.length;
     this.totalRooms = this.roomsData.length;
   };
+
+  // findGuestByProperty(property, value) {
+  //   return this.findDataByProperty("usersData", property, value)[0];
+  // };
+
+  findDataByProperty(dataSet, property, value) {
+    return this[dataSet].filter(data => data[property] === value);
+  };
+
+  findRoom(roomNum) {//*global
+    return this.findDataByProperty("roomsData", "number", roomNum)[0];
+  };
+
+  findAvailableRooms(bookedRooms) {    
+    const allRooms = this.roomsData;    
+    bookedRooms.map(bookedRoom => allRooms.splice(allRooms.findIndex(room => room.number === bookedRoom.number)), 1);
+    return allRooms
+  };
+
+  findBookings(property, value, fnDate) {//*global
+    
+    const sortChronically = (data) => {
+      //console.log('data[0]: ', data[0]);
+      return data.sort( (a, b) => fnDate(b.date, "sort") - fnDate(a.date, "sort") )
+    };
+
+    let bookings = this.findDataByProperty("bookingsData", property, value);
+      //console.log('bookings.length: ', bookings.length);
+    bookings.map(booking => {
+      booking.room = this.findRoom(booking.roomNumber);
+      return booking
+    });
+    //console.log('bookings[0].room: ', bookings[0].room);
+    return sortChronically(bookings);
+  };
+
+  sortByDate(data, date, fnDate) {
+    let pastBookings = [], upcomingBookings = [];
+    const sortDate = fnDate(date, "sort");
+    data.map(booking => {
+      fnDate(booking.date, "sort") <= sortDate ? pastBookings.push(booking) : upcomingBookings.push(booking);  
+    }); 
+    return [{name: "upcoming-bookings", data: upcomingBookings}, {name: "past-bookings", data: pastBookings}];
+  };
+
+  calculateAmountTotals(data) {    
+    //console.log('data[0].room: ', data[0].room);
+    let sum = 0;
+    data.map(booking => {
+      const room = this.findDataByProperty("roomsData", "number", parseInt(booking.roomNumber));
+      sum += room[0].costPerNight;
+    });
+    return sum;  
+  };
+
+  calculatePercentage(num, total) {
+    return num / total;
+  };
+
+
 
   // validateUser(user) {
   //   let isValid = false, found;
@@ -47,46 +107,8 @@ class HotelData {
   //   return isValid;
   // }
 
-  findGuestByProperty(property, value) {
-    return this.findDataByProperty("usersData", property, value)[0];
-  }
 
-  findBookings(property, value) {//*global
-    let bookings = this.findDataByProperty("bookingsData", property, value)
-    .map(booking => {
-      booking.room = this.findRoom(booking.roomNumber);
-      return booking
-    });
-    console.log('bookings @findBookings: ', bookings);
-    return bookings
-  };
   
-  findRoom(roomNum) {//*global
-    return this.findDataByProperty("roomsData", "number", roomNum)[0];
-  };
-
-  calculateAmountTotals(data) {    
-    let sum = 0;
-    data.map(booking => {
-      const room = this.findDataByProperty("roomsData", "number", parseInt(booking.roomNumber));
-      sum += room[0].costPerNight;
-    });
-    return sum;  
-  }
-
-  calculatePercentage(num, total) {
-    return num / total;
-  }
-
-  findAvailableRooms(bookedRooms) {    
-    const allRooms = this.roomsData;    
-    bookedRooms.map(bookedRoom => allRooms.splice(allRooms.findIndex(room => room.number === bookedRoom.number)), 1);
-    return allRooms
-  }
-
-  findDataByProperty(dataSet, property, value) {
-    return this[dataSet].filter(data => data[property] === value);
-  }
 }
 
 export default HotelData;
--- test/HotelData-test.js
+++ test/HotelData-test.js
@@ -1,2 +1,119 @@
 import chai from 'chai';
 const expect = chai.expect;
+
+import HotelData from "../src/class/data/HotelData.js";
+import sampleBookings from "./testData/bookings-sample.js";
+import sampleRooms from "./testData/rooms-sample.js";
+import sampleUsers from "./testData/users-sample.js";
+
+describe('HotelData', function() {
+  let guest; // data, user, user2;
+  
+  function formatDate(date, style) {
+    const dayjs = require('dayjs');
+    dayjs().format();
+    return style === 'sort' ? dayjs(date).format("YYYYMMDD")
+      : style === 'words' ? dayjs(date).format("MMMM D, YYYY") 
+      : style === 'numbers' ? dayjs(date).format("MM/DD/YYYY")
+      : style === 'min' ? dayjs(date).format("YYYY-MM-DD")
+      : dayjs(date).format("YYYY/MM/DD"); 
+  };
+  
+  const sampleData = {usersData: sampleUsers, bookingsData: sampleBookings, roomsData: sampleRooms};
+  const hotelData = new HotelData(sampleData);
+
+  const date = "2021/05/20";
+  //guest = new HotelData({id: 1, name: "Sue"}, date);
+
+
+  it('should be a function', function() {
+    expect(HotelData).to.be.a("function");
+  });
+
+  it('should be an instance of HotelData', function() {
+    //data = {id: 1, name: "Sue"};
+    expect(hotelData).to.be.an.instanceOf(HotelData);
+  });
+
+  it('Should not require an argument to create a new hotelData', function() {
+    expect( () => new HotelData() ).to.not.throw(Error);
+  });
+
+  it('Should have empty array property values for data if no arguement is given for the hotelData', function() {
+    const noArguments = new HotelData();
+    expect(noArguments.usersData.length).to.equal(0);
+  });
+
+  it('Should have property values equalling 0 for any property value that includes length if no arguement is given for the hotelData', function() {
+    const noArguments = new HotelData();
+    expect(noArguments.totalUsers).to.equal(0);
+  });
+
+  it('Should have a "usersData", "bookingsData", "roomsData", "totalUsers", and "totalRooms" property', function() {
+    //hotelData = new hotelData({id: 1, name: "Sue"}, "2021/05/03");
+    const hasUsersData = hotelData.usersData ? true : false;
+    expect(hasUsersData).to.equal(true);
+    
+    const hasBookingsData = hotelData.bookingsData ? true : false;
+    expect(hasBookingsData).to.equal(true);
+    
+    const hasRoomsData = hotelData.roomsData ? true : false;
+    expect(hasRoomsData).to.equal(true);
+    
+    const hasTotalUsers = hotelData.totalUsers ? true : false;
+    expect(hasTotalUsers).to.equal(true);
+    
+    const hasTotalRooms = hotelData.totalRooms ? true : false;
+    expect(hasTotalRooms).to.equal(true);
+
+  });
+  describe('findDataByProperty', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.findDataByProperty).to.be.a("function");
+    });
+  });
+
+  describe('findRoom', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.findRoom).to.be.a("function");
+    });
+  });
+
+  describe('findAvailableRooms', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.findAvailableRooms).to.be.a("function");
+    });
+  });
+
+  describe('findBookings', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.findBookings).to.be.a("function");
+    });
+  });  
+
+  describe('sortByDate', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.sortByDate).to.be.a("function");
+    });
+  });
+
+  describe('calculateAmountTotals', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.calculateAmountTotals).to.be.a("function");
+    });
+  });
+
+  describe('calculatePercentage', function() {
+    it('should be a function', function() {
+      //execution;
+      expect(hotelData.calculatePercentage).to.be.a("function");
+    });
+  });
+
+});
